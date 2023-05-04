8be2a92ff068c7fc570e31c2ff9fe2fd1afd1ba6
Chaoqian Xu <chaoranxu@gmail.com>
2023-05-04 18:05:38 +0800

    refactor: add ws suffix

--- src/connection.ts
+++ src/connection.ts
@@ -304,9 +304,9 @@ export class Connection extends Listenable {
 
   private _buildUrl() {
     if (this._options.sslEnabled) {
-      return `wss://${this._endpoint}`;
+      return `wss://${this._endpoint}/ws`;
     } else {
-      return `ws://${this._endpoint}`;
+      return `ws://${this._endpoint}/ws`;
     }
   }
 
