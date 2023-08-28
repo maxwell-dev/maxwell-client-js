19fe4a15ad7271a715654974124877b039a8b339
Chaoqian Xu <chaoranxu@gmail.com>
2023-08-28 16:54:31 +0800

    chore: update localstore

--- examples/requester.js
+++ examples/requester.js
@@ -9,7 +9,7 @@ async function loop() {
   try {
     let p = client.request("/hello",  {"content": "world"}, { sourceEnabled: true });
     const result = await p;
-    console.log(`Received result: ${result.length}`);
+    console.log(`Received result: `, result);
   } catch (reason) {
     console.error(`Error occured: ${reason.stack}`);
   }
@@ -30,11 +30,11 @@ async function runOnce() {
   }
   console.timeEnd("answer time@runOnce-1");
 
-  console.time("answer time@runOnce-n");
-  for (let i = 0; i < 1; i++) {
-    await Promise.all([loop(), loop(), loop(), loop(), loop()]);
-  }
-  console.timeEnd("answer time@runOnce-n");
+  // console.time("answer time@runOnce-n");
+  // for (let i = 0; i < 1; i++) {
+  //   await Promise.all([loop(), loop(), loop(), loop(), loop()]);
+  // }
+  // console.timeEnd("answer time@runOnce-n");
 }
 
 
@@ -43,6 +43,7 @@ async function runOnce() {
   console.time("answer time@run");
   for (let i = 0; i < 100000000; i++) {
     await runOnce();
+    await new Promise(r => setTimeout(r, 2000));
   }
   console.timeEnd("answer time@run");
 })()
